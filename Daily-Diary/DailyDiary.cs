47345087511f4e24d5f8842e49cdf9f7cbf44dfe
Nory9 <noormisk94@gmail.com>
2024-06-28 15:58:11 +0300

    add mydiary file

--- Daily-Diary/DailyDiary.cs
+++ Daily-Diary/DailyDiary.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Daily_Diary
 {
-    internal class DailyDiary
+    public class DailyDiary
     {
+
+        public void read_Diary() { 
+        string path= "../../../mydiary.txt";
+            string dairy = File.ReadAllText(path);
+            Console.WriteLine(dairy);
+
+
+        }
+    
     }
-}
+
+  }
+
+
