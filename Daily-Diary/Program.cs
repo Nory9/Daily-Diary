881843d64a27f2f378a2058f3a4a19729ad1f579
Nory9 <noormisk94@gmail.com>
2024-06-28 20:40:59 +0300

    remove comments

--- Daily-Diary/Program.cs
+++ Daily-Diary/Program.cs
@@ -6,10 +6,6 @@
         {
             // Console.WriteLine("Hello, World!");
             Diary daily = new Diary();
-            // daily.read_Diary();
-            // daily.Write_Diary();
-            //daily.Read_Diary();
-            // daily.Search_entry();
             daily.Diary_Manager();
 
         }
